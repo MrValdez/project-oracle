b095a0ddc7433c8330a0f201271441e817c6e3dd
MrValdez <shunyvaldez@gmail.com>
2019-10-19 17:27:39 +0800

    added sample tips to extract. printed tips as json format

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def extract():
-    pass
+import json
+
+def extract(filename):
+    f = open(filename)
+    data = f.read()
+    f.close()
+
+    data = json.loads(data)
+    return data
 
 if __name__ == "__main__":
-    result = extract()
-    print(result)
+    results = extract(filename="data.txt")
+    for result in results:
+        print(result)
